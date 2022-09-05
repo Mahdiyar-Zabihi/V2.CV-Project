e897d839cd12e68b9e33db867a5f0c84cd844681
Mahdiyar-Zabihi <105593158+Mahdiyar-Zabihi@users.noreply.github.com>
2022-09-05 18:45:09 +0430

    TailWind & Font Config.ConfigFile,GitIgnore,RunCmd

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports= {
+  darkMode:'class',
+  content:['./Src/**/*.{html,js}','./Src/*.{html,js}','./Src/Index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
