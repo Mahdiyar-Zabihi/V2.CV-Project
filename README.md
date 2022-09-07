e6cec8627f2a555a929f62a84ecad44daf4462f1
Mahdiyar-Zabihi <105593158+Mahdiyar-Zabihi@users.noreply.github.com>
2022-09-07 21:50:08 +0430

    Bug fix for Skills components. Skills component done ( full )

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Version2. CV-Project
+
 Version2 Of CV-Project With Tailwind.CSS
 
-Idea for colors and layouts in Here[https://www.template.net/editable/2443/free-surveyor-cv]
+Idea for colors and layouts in [Here](https://www.template.net/editable/2443/free-surveyor-cv).
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,25 @@ module.exports= {
   darkMode:'class',
   content:['./Src/**/*.{html,js,jsx}','./Src/*.{html,js,jsx}','./Src/Index.html'],
   theme: {
-    extend: {
-      colors:{
-        'primaryColor':{
-          dark:'#198a82',
-          main:'#67bcb6',
-        },
-        'secondaryColor':'#ca9a5f',
+    extend:
+      {
+        colors:
+          {
+            'primaryColor':{
+              dark:'#198a82',
+              main:'#67bcb6',
+            },
+            'secondaryColor':'#ca9a5f',
+          },
+        rotate:
+          {
+            full:'360deg'
+          },
+        boxShadow:
+          {
+            'CustomInset':'inset 0 0 6px lighten($color:#0e0d0d7b,$amount:3)'
+          },
       },
-      rotate:{
-        full:'360deg'
-      },
-      boxShadow:{
-        'CustomInset':'inset 0 0 6px lighten($color:#0e0d0d7b,$amount:3)'
-      }
-    },
   },
   plugins: [],
 };
