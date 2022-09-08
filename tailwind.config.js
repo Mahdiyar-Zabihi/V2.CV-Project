08979261787a6fcaf9471985efa7b7f479788819
Mahdiyar-Zabihi <105593158+Mahdiyar-Zabihi@users.noreply.github.com>
2022-09-08 12:24:49 +0430

    New animations, Bug fix. last commit ( full done )

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@
 module.exports= {
   darkMode:'class',
   content:['./Src/**/*.{html,js,jsx}','./Src/*.{html,js,jsx}','./Src/Index.html'],
-  theme: {
+  theme:{
     extend:
       {
         colors:
@@ -22,9 +22,23 @@ module.exports= {
           },
         boxShadow:
           {
-            'CustomInset':'inset 0 0 6px lighten($color:#0e0d0d7b,$amount:3)'
+            'CustomInset':'inset 0 0 6px #c3d6d5',
+          },
+        animation:
+          {
+            'ShowOnTop':'ShowOnTop 0.6s ease-out',
+            'ShowOnBottom':'ShowOnBottom 0.6s ease-out',
+            'ShowOnRight':'ShowOnRight 0.6s ease-out',
+            'ShowOnLeft':'ShowOnLeft 0.6s ease-out',
+          },
+        keyframes:
+          {
+            'ShowOnTop':{'0%':{'opacity':'0','transform':'translateY(-30%)',},'100%':{'opacity':'1',}},
+            'ShowOnBottom':{'0%':{'opacity':'0','transform':'translateY(30%)',},'100%':{'opacity':'1',}},
+            'ShowOnRight':{'0%':{'opacity':'0','transform':'translateX(30%)',},'100%':{'opacity':'1',}},
+            'ShowOnLeft':{'0%':{'opacity':'0','transform':'translateX(-30%)',},'100%':{'opacity':'1',},},
           },
       },
   },
-  plugins: [],
+  plugins:[],
 };
